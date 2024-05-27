6cd3ef515550da9afa18e19acb78bdc8ca3be4f3
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-05-27 12:00:38 -0700

    trying to get it to render correctly

--- src/components/PortfolioPage/index.jsx
+++ src/components/PortfolioPage/index.jsx
@@ -9,7 +9,7 @@ export default function PortfolioPage() {
         <div className="projects">
           <a href='https://github.com/Odesii/dataRebels' target='_blank' rel='noopener noreferrer'>
             <span id='titles'>Turn-based Game</span>
-            <img src="public/datarebelsSS.JPG" alt="Turn-based Game Screenshot" />
+            <img src="./public\datarebelsSS.JPG" alt="Turn-based Game Screenshot" />
           </a>
           <a id='deployed' href="https://datarebels.onrender.com/login" target='_blank' rel='noopener noreferrer'>Deployed Link</a>
         </div>
@@ -17,7 +17,7 @@ export default function PortfolioPage() {
         <div className="projects">
           <a href="https://github.com/RTAKA808/theTextEditor" target='_blank'>
             <span id='titles'>Text Editor</span>
-            <img src="public/texteditor.JPG" alt="Text Editor Screenshot" />
+            <img src="./public\texteditor.JPG" alt="Text Editor Screenshot" />
           </a>
           <a id='deployed' href="https://thetexteditor-q55t.onrender.com/" target='_blank'>Deployed Link</a>
         </div>
@@ -25,30 +25,30 @@ export default function PortfolioPage() {
         <div className="projects">
           <a href="https://github.com/RTAKA808/postgressexpress" target='_blank' >
             <span id='titles'>PostgresSql</span>
-            <img src="public/postgressexpressscreenshot.JPG" alt="PostgresSql Screenshot" />
+            <img src="./public\postgressexpressscreenshot.JPG" alt="PostgresSql Screenshot" />
           </a>
         </div>
 
         <div className="projects">
           <a href="https://github.com/RTAKA808/ORMsqlize" target='_blank' >
             <span id='titles'>Using Sequelize</span>
-            <img src="public/ormss.JPG" alt="Sequelize Screenshot" />
+            <img src="./public\ormss.JPG" alt="Sequelize Screenshot" />
           </a>
         </div>
 
         <div className="projects">
           <a href="https://github.com/RTAKA808/noSQLzone" target='_blank' >
             <span id='titles'>MongoDb and Mongoose</span>
-            <img src="public/image.png" alt="MongoDb and Mongoose Screenshot" />
+            <img src="./public\image.png" alt="MongoDb and Mongoose Screenshot" />
           </a>
         </div>
 
         <div className="projects">
           <a href="https://github.com/RTAKA808/blogbars" target='_blank'>
             <span id='titles'>Handlebars Blogsite with Authentication</span>
-            <img src="public/blogbarsss.JPG" alt="Blogsite Screenshot" />
+            <img src="./public\blogbarsss.JPG" alt="Blogsite Screenshot" />
           </a>
-          <a id='deployed' href="https://blogbars.onrender.com/" target='_blank' rel='noopener noreferrer'>Deployed Link</a>
+          <a id='deployed' href="https://blogbars.onrender.com/" target='_blank'>Deployed Link</a>
         </div>
       </div>
     </section>
