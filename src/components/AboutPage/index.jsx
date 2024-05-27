6e3c80d19b2fb8f40643d1b4c86277754435e4a9
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-05-27 12:12:31 -0700

    changed file paths to images for build

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -2,7 +2,7 @@ import styles from './index.css'
 export default function AboutPage() {
   return (
     <div className='container'>
-      <img className='logo'src="./public\RT_Avatar.png" alt="" />
+      <img className='logo'src="./RT_Avatar.png" alt="" />
       <h2>A Little About Me:</h2>
       <p>
         My name is Reyn Takahashi and I am an aspiring Software Engineer with experience with full stack applications. I have experience with modalities that include: HTML, JavaScript, CSS, PostgresSQL, MongoDB, NodeJS,Express, ReactJS, and more… I am always excited to learn new things and it has even become my hobby to build games.
--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -4,9 +4,9 @@ export default function Footer(){
     return (
         <footer>
             <div className='icons'>
-                <a href="https://github.com/RTAKA808" target="_blank"><img src="./public\github.svg" alt="" /></a>
-                <a href="https://www.linkedin.com/in/reyn-takahashi-4b2034302?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BHUU8c9BYR6mJxcaXxoKXXQ%3D%3D" target="_blank"><img src="./public\linkedin.svg" alt="" /></a>
-                <a href="https://discord.gg/ACTHPNyz" target="_blank"><img src="./public\discord.svg" alt="" /></a>
+                <a href="https://github.com/RTAKA808" target="_blank"><img src="./github.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/in/reyn-takahashi-4b2034302?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BHUU8c9BYR6mJxcaXxoKXXQ%3D%3D" target="_blank"><img src="./linkedin.svg" alt="" /></a>
+                <a href="https://discord.gg/ACTHPNyz" target="_blank"><img src="./discord.svg" alt="" /></a>
             </div>
         </footer>
     )
